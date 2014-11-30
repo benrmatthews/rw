9e0e14bba8b9d065cd3220c15ebbe3eb07148295
benrmatthews <ben@montfort.io>
2014-11-30 09:23:38 +0000

    fixed assets

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Rw
   class Application < Rails::Application
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    #config.assets.paths << Rails.root.join("app", "assets", "fonts")
     
     config.generators do |g|
       g.test_framework :rspec,
