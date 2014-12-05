eed7d7ae882514d756d61157d91a5f9f65e7f881
benrmatthews <ben@montfort.io>
2014-12-05 15:22:39 +0000

    Removed X accel request'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,5 @@ Rails.application.configure do
   
   # Getting assets pipeline in order
   config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
   config.assets.compile = true
 end
