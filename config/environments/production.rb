5473b61b69e54e4bb0ed8c44697f4b8b530bf222
benrmatthews <ben@montfort.io>
2014-12-05 15:18:26 +0000

    prepared assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Getting assets pipeline in order
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+  config.assets.compile = true
 end
